225f42eea1fd03729c654606124b23fa11d8aec8
shinklsy <shinklsy@MCD210-PC-07.uccc.uc.edu>
2020-02-06 11:50:13 -0500

    Added objects to main class

--- InClass_2020_02_06/src/main/Main.java
+++ InClass_2020_02_06/src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import bank.Bank;
+
 public class Main {
 
 	public static void main(String[] args) {
+		// Declare and instantiate a Bank, an Online, a BrickAndMortar, and a CafeBranch object
+		Bank fifththird = new Bank();
 		
-
 	}
 
 }
